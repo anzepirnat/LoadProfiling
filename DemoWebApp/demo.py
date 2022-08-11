4622ed102394511cda9291a71b570c268e753239
anzepirnat <87640733+anzepirnat@users.noreply.github.com>
2022-08-11 15:54:59 +0200

    Add files via upload

--- DemoWebApp/demo.py
+++ DemoWebApp/demo.py
@@ -56,7 +56,7 @@ st.write("Elderly care dahsboard is a webapp developed to showcase usability of 
 st.write("It works by studying the routine of the elders electric device usage and then comparing current state to routine state. If current state deviates by the usual routine state enough on two devices at a time, it is recognised as an anomaly.")
 st.write("It is designed to give user an insight into the last anomaly detected, its accuracy and date+time of it as it could mean an elder is unconscious and his life in danger. Accuracy provides the probability of said medical state.")
 st.write("To provide help to the elderly there is a phone number, location and some basic personal information that a caregiver can use to aid.")
-st.write("Beside that the webapp provides the quality of routine, on which the accuracy of our prediction depends on. It also features the best and worst 3 devices by their impact on the quality of routine. We suggest the elser tries to improve on the worst 3. We give our feedback about them in a user friendly manner, describing them with: excellent, very good, good, quite good, decent, usable, barely usable, useless (adjectives are listed descending by their desirability).")
+st.write("Beside that the webapp provides the quality of routine, on which the accuracy of our prediction depends on. It also features the most and least descriptive 3 devices by their impact on the quality of routine. We suggest the elders try to improve on their routine having the most descriptive 3 as an example. We give our feedback on devices in a user friendly manner, describing them with easily understandable graphics.")
 st.subheader("Please start by choosing the house of interest.")
 
 
